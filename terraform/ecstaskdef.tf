86444cfb3c9582cd334214a4af0fa3ee9968a91e
Bishajit Lodh <67888547+Bishajit@users.noreply.github.com>
2022-01-22 04:03:23 -0500

    Update ecstaskdef.tf

--- terraform/ecstaskdef.tf
+++ terraform/ecstaskdef.tf
@@ -16,7 +16,7 @@ resource "aws_ecs_task_definition" "ei_task_definition" {
 [
   {
     "name": "${var.application_name}-application",
-    "image": "829789961825.dkr.ecr.us-east-1.amazonaws.com/circle-ci:latest",
+    "image": "323867645900.dkr.ecr.us-east-1.amazonaws.com/circle-ci:latest",
     "cpu": 1024,
     "memory": 2048,
     "essential": true,
